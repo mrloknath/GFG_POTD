f624ae90c14d62fc76905a4c34196c0b114c94f8
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-03-01 07:38:35 +0530

    Create 01_03_2024(Peak_element).py

---
+++ 2024_03/01_03_2024(Peak_element).py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMaxPosition(self, arr):
+        max_val = arr[0]
+        p = 0
+        for i in range(1, len(arr)):
+            if max_val < arr[i]:
+                max_val = arr[i]
+                p = i
+        return p
+
+    def peakElement(self, arr,n):
+        if n == 1:
+            return 0
+        elif n == 2:
+            return self.findMaxPosition(arr)
+        else:
+            return self.findMaxPosition(arr)
