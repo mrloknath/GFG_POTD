b3b6464e6cfdefc8170cc239cc40372a213decbf
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-03-15 18:07:57 +0530

    Update 15_03_2024(Linked_List_that_is_Sorted_Alternatingly).py

--- 2024_03/15_03_2024(Linked_List_that_is_Sorted_Alternatingly).py
+++ 2024_03/15_03_2024(Linked_List_that_is_Sorted_Alternatingly).py
@@ -1,24 +1,44 @@
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-
-
 class Solution:
-    def sort(self, h1):
-        s = Node(0)
-        d = Node(0)
-        w = s
-        while h1:
-            w.next = h1
-            w = w.next
-            h1 = h1.next
-            w.next = None
-            if h1:
-                t = h1.next
-                h1.next = d.next
-                d.next = h1
-                h1 = t
-        w.next = d.next
-        return s.next
+    def sort(self, head):
+        if not head or not head.next:
+            return head
 
+        # Split the list into two halves
+        mid = self.get_mid(head)
+        left = head
+        right = mid.next
+        mid.next = None
+
+        # Recursively sort the two halves
+        left = self.sort(left)
+        right = self.sort(right)
+
+        # Merge the sorted halves
+        return self.merge(left, right)
+
+    def get_mid(self, head):
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def merge(self, left, right):
+        dummy = Node(0)
+        curr = dummy
+
+        while left and right:
+            if left.data < right.data:
+                curr.next = left
+                left = left.next
+            else:
+                curr.next = right
+                right = right.next
+            curr = curr.next
+
+        if left:
+            curr.next = left
+        elif right:
+            curr.next = right
+
+        return dummy.next
