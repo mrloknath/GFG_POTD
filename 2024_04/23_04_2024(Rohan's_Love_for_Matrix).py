7b50520b29e2b4314d0abb87cbcb907a0d6c196f
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-04-23 17:41:34 +0530

    Update 23_04_2024(Rohan's_Love_for_Matrix).py

--- 2024_04/23_04_2024(Rohan's_Love_for_Matrix).py
+++ 2024_04/23_04_2024(Rohan's_Love_for_Matrix).py
@@ -1,3 +1,5 @@
+Author:Loknath Giri
+
 class Solution:
     def firstElement (self, n):
         # code here 
@@ -5,11 +7,9 @@ class Solution:
             return 1
         a, b = 1, 1
         mod = 1000000007
-        
         for i in range(n - 2):
             c = (a + b) % mod
             a = b % mod
             b = c % mod
-        
         return c % mod
 
