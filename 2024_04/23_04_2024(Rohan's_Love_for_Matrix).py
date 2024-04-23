5598fdad1f079063d76c7adeed41a4cbc3aa6041
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-04-23 07:12:36 +0530

    Create 23_04_2024(Rohan's_Love_for_Matrix).py

---
+++ 2024_04/23_04_2024(Rohan's_Love_for_Matrix).py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstElement (self, n):
+        # code here 
+        if n == 0 or n == 1:
+            return 1
+        a, b = 1, 1
+        mod = 1000000007
+        
+        for i in range(n - 2):
+            c = (a + b) % mod
+            a = b % mod
+            b = c % mod
+        
+        return c % mod
+
