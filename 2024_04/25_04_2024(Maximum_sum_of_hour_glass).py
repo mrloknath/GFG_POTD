0961235f4fd75fd62f44c8a61e1964079271c7cc
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-04-25 07:29:55 +0530

    Create 25_04_2024(Maximum_sum_of_hour_glass).py

---
+++ 2024_04/25_04_2024(Maximum_sum_of_hour_glass).py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxSum(self,n,m,mat):
+        #code here
+        if(n<3 or m<3):
+            return -1
+        ans=-1
+        sum=0
+        for i in range(0,n-2):
+            for j in range(0,m-2):
+                sum=mat[i][j]+mat[i][j+1]+mat[i][j+2]+mat[i+1][j+1]+mat[i+2][j]+mat[i+2][j+1]+mat[i+2][j+2]
+                ans=max(ans,sum)
+        return ans
