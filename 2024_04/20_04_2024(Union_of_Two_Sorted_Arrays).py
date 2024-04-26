d8fa2dc05bf30e074c5202b62838798d5fc6835c
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-04-26 07:05:00 +0530

    Update 20_04_2024(Union_of_Two_Sorted_Arrays).py

--- 2024_04/20_04_2024(Union_of_Two_Sorted_Arrays).py
+++ 2024_04/20_04_2024(Union_of_Two_Sorted_Arrays).py
@@ -2,13 +2,6 @@ class Solution:
     
     #Function to return a list containing the union of the two arrays.
     def findUnion(self,arr1,arr2,n,m):
-        '''
-        :param a: given sorted array a
-        :param n: size of sorted array a
-        :param b: given sorted array b
-        :param m: size of sorted array b
-        :return:  The union of both arrays as a list
-        '''
         # code here 
         s = set()
         for i in range(n):
