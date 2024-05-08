7f5bea04d5e119b06f4a4131e5fe68142b81cf52
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-05-08 10:46:27 +0530

    Create 08_05_2024(Root_to_Leaf_Paths).py

---
+++ 2024_05/08_05_2024(Root_to_Leaf_Paths).py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def __init__(self):
+        self.all_paths = []
+    def Paths(self, root : Optional['Node']) -> List[List[int]]:
+        # code here
+        def get_paths(node, path):
+            if node.left:
+                get_paths(node.left, path + [node.data])
+            if node.right:
+                get_paths(node.right, path + [node.data])
+            
+            if not node.left and not node.right:
+                self.all_paths.append(path + [node.data])
+        if not root:
+            return []
+        path = []
+        get_paths(root, path)
+        return self.all_paths
