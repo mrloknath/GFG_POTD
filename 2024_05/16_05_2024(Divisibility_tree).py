95e3356295679a071fc9e9ad4929e56033603e8d
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-05-16 07:38:27 +0530

    Create 16_05_2024(Divisibility_tree).py

---
+++ 2024_05/16_05_2024(Divisibility_tree).py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def __init__(self):
+        self.ans = 0
+    
+    def dfs(self, adj, vis, start):
+        vis[start] = 1
+        cnt = 0
+        
+        for neighbor in adj[start]:
+            if not vis[neighbor]:
+                res = self.dfs(adj, vis, neighbor)
+                if res % 2 == 0:
+                    self.ans += 1
+                else:
+                    cnt += res
+        
+        return cnt + 1
+    def minimumEdgeRemove(self, n : int, edges : List[List[int]]) -> int:
+        # code here
+        adj = [[] for _ in range(n)]
+        for edge in edges:
+            adj[edge[0] - 1].append(edge[1] - 1)
+            adj[edge[1] - 1].append(edge[0] - 1)
+        
+        vis = [0] * n
+        
+        self.dfs(adj, vis, 0)
+        
+        return self.ans
+        
