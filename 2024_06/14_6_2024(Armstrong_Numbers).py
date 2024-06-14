8e96082945e8419bac62e5e1360c5158a050714f
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-06-14 07:40:01 +0530

    Delete 2024_06/14_06_2024(Armstrong_Numbers).py

--- 2024_06/14_6_2024(Armstrong_Numbers).py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def armstrongNumber(self, n: int) -> str:
-        n1 = n
-        digits = len(str(n))
-        ans = sum(int(digit) ** digits for digit in str(n))
-        if ans == n1:
-            return "Yes"
-        return "No"
