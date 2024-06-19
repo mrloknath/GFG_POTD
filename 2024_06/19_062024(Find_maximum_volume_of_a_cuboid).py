0ea235902c8f6260119201618897adb27b61b9b3
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-06-19 05:52:28 +0530

    Delete 2024_06/19_06_2024(Find_maximum_volume_of_a_cuboid).py

--- 2024_06/19_062024(Find_maximum_volume_of_a_cuboid).py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maxVolume(self, perimeter, area):
-        #code here
-        length = (perimeter - math.sqrt(perimeter * perimeter - 24 *  area)) / 12
-        height = perimeter / 4 - 2 * length
-        volume = length * length * height
-        return volume
