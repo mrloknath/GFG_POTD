b3e439beb9fbb25604ffaa21f6cf8dcb1a738b60
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-07-16 06:21:55 +0530

    Create 16_07_2024(Remaining_String).py

---
+++ 2024_07/16_07_2024(Remaining_String).py
@@ -0,0 +1,7 @@
+class Solution:
+    def printString(self, s, ch, count):
+        # code here
+        for i, c in enumerate(s):
+            if c == ch: count -= 1
+            if count == 0: return s[i+1:]
+        return ""
