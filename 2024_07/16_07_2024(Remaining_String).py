abbf497f5aa52e7538fee0ac50eaf6ec378ba1df
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-07-17 10:51:46 +0530

    Update 16_07_2024(Remaining_String).py

--- 2024_07/16_07_2024(Remaining_String).py
+++ 2024_07/16_07_2024(Remaining_String).py
@@ -1,6 +1,6 @@
 class Solution:
     def printString(self, s, ch, count):
-        # code here
+        # code here 
         for i, c in enumerate(s):
             if c == ch: count -= 1
             if count == 0: return s[i+1:]
