2d361938e86b4092ed7d35d76ec258bf112d26e7
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-07-23 09:43:42 +0530

    Create 23_07_2024(Merge_two_BST).py

---
+++ 2024_07/23_07_2024(Merge_two_BST 's).py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, root1, root2):
+        def dfs(node, arr):
+            if not node:
+                return
+            dfs(node.left, arr)
+            arr.append(node.data)
+            dfs(node.right, arr)
+            
+        bst1, bst2 = [], []
+        dfs(root1, bst1)
+        dfs(root2, bst2)
+        
+        return sorted(bst1 + bst2)
