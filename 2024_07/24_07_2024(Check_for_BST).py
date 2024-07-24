ceec27257088756102dd377b23b549d00b094e8e
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-07-24 13:20:48 +0530

    Create 24_07_2024(Check_for_BST).py

---
+++ 2024_07/24_07_2024(Check_for_BST).py
@@ -0,0 +1,12 @@
+class Solution:
+    def is_bst(self, node, min_val, max_val):
+        if node is None:
+            return True
+        if not (min_val < node.data < max_val):
+            return False
+        return self.is_bst(node.left, min_val, node.data) and self.is_bst(node.right, node.data, max_val)
+        
+    def isBST(self, root):
+        #code here
+        return self.is_bst(root, float('-inf'), float('inf'))
+
