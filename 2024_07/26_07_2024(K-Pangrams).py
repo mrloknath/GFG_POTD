e4b28dc66d099108c7e32a729eea0326215adc07
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-07-26 21:22:21 +0530

    Update 26_07_2024(K-Pangrams).py

--- 2024_07/26_07_2024(K-Pangrams).py
+++ 2024_07/26_07_2024(K-Pangrams).py
@@ -2,6 +2,7 @@ from collections import Counter
 
 class Solution:
     def kPangram(self,string, k):
+        #code here
         string = string.replace(' ', '')
         mp = Counter(string)
         cnt, total = len(mp.keys()), sum(mp.values())
