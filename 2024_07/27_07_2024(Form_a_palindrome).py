019d539532b5e1cc33e2c3335b848faf0036ac22
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-07-27 08:52:43 +0530

    Create 27_07_2024(Form_a_palindrome).py

---
+++ 2024_07/27_07_2024(Form_a_palindrome).py
@@ -0,0 +1,16 @@
+class Solution:
+    def countMin(self, str):
+        m=len(str)
+        from sys import setrecursionlimit
+        setrecursionlimit(2*10**3)
+        from functools import lru_cache
+        @lru_cache(None)
+        def lcs(i=0,j=m-1):
+            nonlocal str
+            if i>=j:
+                return i==j
+            if str[i]==str[j]:
+                return lcs(i+1,j-1)+2
+            mx=max(lcs(i+1,j),lcs(i,j-1))
+            return mx
+        return m-lcs()
