fa4eb707cce1d874a582984cb5e2a8549f450969
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-07-29 07:40:05 +0530

    Create 29_07_2024(Row_with_max_1s).py

---
+++ 2024_07/29_07_2024(Row_with_max_1s).py
@@ -0,0 +1,10 @@
+class Solution:
+    def rowWithMax1s(self, arr):
+        max_count, max_index = 0, -1
+        for i in range(len(arr)):
+            if arr[i][-1] > max_count:
+                max_count = arr[i][-1]
+                max_index = i
+            elif arr[i][-1] == max_count:
+                max_index = i if sum(arr[i]) > sum(arr[max_index]) else max_index
+        return max_index
