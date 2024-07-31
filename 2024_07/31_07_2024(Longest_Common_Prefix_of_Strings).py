65f0bee17eae15a5fa5820afebf9548694be8331
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-07-31 06:36:55 +0530

    Create 31_07_2024(Longest_Common_Prefix_of_Strings).py

---
+++ 2024_07/31_07_2024(Longest_Common_Prefix_of_Strings).py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, arr):
+        # code here
+        pre=arr[0]
+        for word in arr[1:]:
+            i=0
+            for c in word:
+                if i>=len(pre) or c!= pre[i]:
+                    break
+                i+=1
+            pre=pre[:i]
+        return pre if len(pre)>0 else -1
