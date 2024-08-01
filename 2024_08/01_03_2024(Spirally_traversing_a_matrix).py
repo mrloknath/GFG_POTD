bbf238c4acc221eb496fc628dd79d827a5b9ac8d
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-08-01 07:33:47 +0530

    Create 01_03_2024(Spirally_traversing_a_matrix).py

---
+++ 2024_08/01_03_2024(Spirally_traversing_a_matrix).py
@@ -0,0 +1,16 @@
+class Solution:
+    def spirallyTraverse(self, matrix):
+        m = len(matrix)
+        n = len(matrix[0])
+        k = 2 * min(m, n)
+        dx = [0, 1, 0, -1]
+        dy = [1, 0, -1, 0]
+        x, y = 0, -1
+        res = []
+        for d in range(k):
+            for i in range(n):
+                x += dx[d % 4]
+                y += dy[d % 4]
+                res.append(matrix[x][y])
+            m, n = n, m - 1
+        return res
