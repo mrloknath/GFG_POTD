153b88ea7da22356310041765661f784c70dc0f4
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-08-02 08:10:50 +0530

    Create 02_08_2024().py

---
+++ 2024_08/02_08_2024().py
@@ -0,0 +1,16 @@
+class Solution:
+	def editDistance(self, str1, str3):
+        m, n = len(str1), len(str3)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(m):
+            dp[i+1][0] = i+1
+        for i in range(n):
+            dp[0][i+1] = i+1
+            
+        for i in range(m):
+            for j in range(n):
+                if str1[i] == str3[j]:
+                    dp[i+1][j+1] = dp[i][j]
+                else:
+                    dp[i+1][j+1] = min(dp[i][j+1], dp[i+1][j], dp[i][j])+1
+        return dp[-1][-1]
