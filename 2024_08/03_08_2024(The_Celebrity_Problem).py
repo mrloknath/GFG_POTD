38891098d1f0d68c88e7ba2184a91ad81312520d
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-08-03 07:55:14 +0530

    Create 03_08_2024(The_Celebrity_Problem).py

---
+++ 2024_08/03_08_2024(The_Celebrity_Problem).py
@@ -0,0 +1,8 @@
+class Solution:
+    def celebrity(self, mat):
+        # code here
+        n = len(mat)
+        for i, r in enumerate(mat):
+            if sum(r) == 0 and sum([mat[j][i] for j in range(n)]) == n-1:
+                return i
+        return -1
