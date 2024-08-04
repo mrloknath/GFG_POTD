f8333f78a614a6bbb82e829ce02c5f2d35495688
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-08-04 09:12:01 +0530

    Create 04_08_2024(N_meetings_in_one_room).py

---
+++ 2024_08/04_08_2024(N_meetings_in_one_room).py
@@ -0,0 +1,15 @@
+class Solution:
+    
+    #Function to find the maximum number of meetings that can
+    #be performed in a meeting room.
+    def maximumMeetings(self,n,start,end):
+        # code here
+        meetings = list(zip(start, end))
+        meetings.sort(key=lambda x: x[1])
+        last_end_time = -1
+        count = 0
+        for meeting in meetings:
+            if meeting[0] > last_end_time:
+                count += 1
+                last_end_time = meeting[1]
+        return count
