c1067a244650c636181fbc24842f10b0438ca0cf
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-08-07 19:54:40 +0530

    Update 07_08_2024(K-th_element_of_two_Arrays).py

--- 2024_08/07_08_2024(K-th_element_of_two_Arrays).py
+++ 2024_08/07_08_2024(K-th_element_of_two_Arrays).py
@@ -10,10 +10,8 @@ class Solution:
                 return min(arr1[i], arr2[j])
             k1 = min(m, k//2)
             k2 = k-k1
-            
             if arr1[i+k1-1] < arr2[j+k2-1]:
                 return kth(arr1, arr2, i+k1, j, m-k1, n, k-k1)
             else:
                 return kth(arr1, arr2, i, j+k2, m, n-k2, k-k2)
-            
         return kth(arr1, arr2, 0, 0, len(arr1), len(arr2), k)
