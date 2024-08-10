b116d0daa8fb4eea3bf8a735bd8287db8fca7195
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-08-10 07:29:01 +0530

    Create 10_08_2024(Rotate_a_Linked_List).py

---
+++ 2024_08/10_08_2024(Rotate_a_Linked_List).py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, head, k):
+        current = head
+        reversedPart = None  
+        remainingRotations = k
+        lastNode = head
+        while lastNode.next is not None:
+            lastNode = lastNode.next
+        while remainingRotations != 0 and current is not None:
+            nextNode = current.next 
+            head = nextNode  
+            current.next = reversedPart  
+            lastNode.next = current  
+            lastNode = lastNode.next  
+            current = nextNode  
+            remainingRotations -= 1  
+        return head  
