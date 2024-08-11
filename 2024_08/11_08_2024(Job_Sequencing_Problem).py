6508e626516e2cd205facf07e2112df10e2d761f
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-08-11 18:22:11 +0530

    Update 11_08_2024(Job_Sequencing_Problem).py

--- 2024_08/11_08_2024(Job_Sequencing_Problem).py
+++ 2024_08/11_08_2024(Job_Sequencing_Problem).py
@@ -6,9 +6,7 @@ class Solution:
         for i in sorted_Jobs:
             idx = i.deadline-1
             while idx >= 0 and timeline[idx] != -1: idx -= 1
-            
             if idx >= 0:
-                
                 timeline[idx] = i.id
                 sm_ct,ct = sm_ct+i.profit,ct+1
                 
