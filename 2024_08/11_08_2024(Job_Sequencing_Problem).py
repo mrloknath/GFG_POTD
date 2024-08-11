2c6b4e7cfcd5503a6c78076dd01983ef5021b7dd
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-08-11 18:20:42 +0530

    Create 11_08_2024(Job_Sequencing_Problem).py

---
+++ 2024_08/11_08_2024(Job_Sequencing_Problem).py
@@ -0,0 +1,21 @@
+class Solution:
+    
+    def JobScheduling(self,Jobs,n):
+        
+        sorted_Jobs = sorted(Jobs,key=lambda x: x.profit, reverse=True)
+        
+        timeline = [-1]*n
+        sm_ct,ct = 0,0
+        
+        for i in sorted_Jobs:
+            
+            idx = i.deadline-1
+            
+            while idx >= 0 and timeline[idx] != -1: idx -= 1
+            
+            if idx >= 0:
+                
+                timeline[idx] = i.id
+                sm_ct,ct = sm_ct+i.profit,ct+1
+                
+        return [ct,sm_ct]
