ea8746735b73686e6ffc62921fc78d23395666e2
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-08-12 07:41:58 +0530

    Update 12_08_2024(Median_of_two_sorted_arrays).py

--- 2024_08/12_08_2024(Median_of_two_sorted_arrays).py
+++ 2024_08/12_08_2024(Median_of_two_sorted_arrays).py
@@ -1,7 +1,7 @@
 class Solution:
     def sum_of_middle_elements(self, arr1, arr2):
         # code here
-        ans=arr1+arr2 #merges two arrays together
+        ans=arr1+arr2
         ans.sort()
         n=len(ans)
         return ans[n//2-1]+ans[n//2]
