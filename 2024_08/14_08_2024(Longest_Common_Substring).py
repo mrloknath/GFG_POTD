6a0451f7c4cbc5827c0464adb547d5e687a51d86
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-08-14 08:29:22 +0530

    Create 14_08_2024(Longest_Common_Substring).py

---
+++ 2024_08/14_08_2024(Longest_Common_Substring).py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonSubstr(self,str1, str2):
+        m = len(str1)
+        n = len(str2)
+        LCSuff = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        result = 0  
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if str1[i - 1] == str2[j - 1]:
+                    LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1
+                    result = max(result, LCSuff[i][j])
+                else:
+                    LCSuff[i][j] = 0
+        return result
