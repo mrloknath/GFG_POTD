9371f6001703c44c2a4551cfb50302dd51b2995e
Loknath Giri <97374826+mrloknath@users.noreply.github.com>
2024-09-26 08:12:00 +0530

    Create 26_09_2024(Roof_Top).py

---
+++ 2024_09/26_09_2024(Roof_Top).py
@@ -0,0 +1,8 @@
+class Solution:.
+    def maxStep(self, arr):
+        ct = mx = 0
+        for i in range(1,len(arr)):
+            if arr[i] > arr[i-1]: ct += 1
+            else: ct = 0
+            mx = max(mx,ct)
+        return mx  
